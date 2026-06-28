08841f4b506aebef75b82efd19e7aed909be5288
tadic011 <tadic011@gmail.com>
2026-06-28 14:29:57 +0200

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set GRADLE_VERSION=8.9
-set GRADLE_HOME=%USERPROFILE%\.gradle\custom-gradle-%GRADLE_VERSION%
-if exist "%GRADLE_HOME%\bin\gradle.bat" goto run
-powershell -ExecutionPolicy Bypass -Command "New-Item -ItemType Directory -Force -Path $env:USERPROFILE\.gradle | Out-Null; Invoke-WebRequest -Uri https://services.gradle.org/distributions/gradle-%GRADLE_VERSION%-bin.zip -OutFile $env:USERPROFILE\.gradle\gradle-%GRADLE_VERSION%-bin.zip; Expand-Archive -Force $env:USERPROFILE\.gradle\gradle-%GRADLE_VERSION%-bin.zip $env:USERPROFILE\.gradle; Rename-Item -Force $env:USERPROFILE\.gradle\gradle-%GRADLE_VERSION% custom-gradle-%GRADLE_VERSION%"
-:run
-"%GRADLE_HOME%\bin\gradle.bat" %*
